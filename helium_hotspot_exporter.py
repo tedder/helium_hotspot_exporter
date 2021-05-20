2be2beb11a230fed77a1cf1a104ca983bfbc6e13
ted <ted@timmons.me>
2021-05-20 14:18:39 -0700

    add account-related stuff
    straighten out some acct vs hotspot naming, call things 'account' instead of 'owner', etc.

--- helium_hotspot_exporter.py
+++ helium_hotspot_exporter.py
@@ -36,9 +36,12 @@ HOTSPOT_YES_LISTEN_ADDRS = prometheus_client.Gauge('helium_hotspot_has_listen_ad
 HOTSPOT_EXIST_EPOCH = prometheus_client.Gauge('helium_hotspot_existence_epoch_seconds', 'Time that hotspot has been in existence', ['hotspot_address', 'hotspot_name'])
 HOTSPOT_HEIGHT = prometheus_client.Gauge('helium_hotspot_heights', 'Blockchain height of various states', ['hotspot_address', 'hotspot_name', 'state_type'])
 HOTSPOT_SCALE = prometheus_client.Gauge('helium_hotspot_reward_scale', 'Reward scale of hotspot', ['hotspot_address', 'hotspot_name'])
+HOTSPOT_ACTIVITY_COUNT = prometheus_client.Gauge('helium_hotspot_activity_count', 'Counts for various activities by hotspot', ['hotspot_address','hotspot_name','activity_type'])
 HOTSPOT_SLOW_UPDATE_EPOCH = prometheus_client.Gauge('helium_hotspot_slow_update_epoch_seconds', 'Time since slow updates were last run', ['hotspot_address', 'hotspot_name'])
 SLOW_NEARBY_HOTSPOTS = prometheus_client.Gauge('helium_hotspot_nearby_count', 'Number of hotspots nearby. Updated infrequently.', ['hotspot_address', 'hotspot_name', 'distance_m'])
-
+ACCOUNT_BALANCE = prometheus_client.Gauge('helium_account_balance', 'Token balance for a given account', ['account_address', 'token_type'])
+ACCOUNT_BLOCK = prometheus_client.Gauge('helium_account_block_height', 'Block height the account was last updated', ['account_address'])
+ACCOUNT_ACTIVITY_COUNT = prometheus_client.Gauge('helium_account_activity_count', 'Counts for various activities by account', ['account_address','activity_type'])
 
 
 SLOW_DATA = {}
@@ -86,8 +89,8 @@ def req_get_json(url):
 def normalize_hotspot_name(hname):
   return hname.strip().lower().replace(' ', '-')
 
-def get_hotspots_by_owner(owner_addr):
-  ret = req_get_json(mkurl('accounts/', owner_addr, '/hotspots'))
+def get_hotspots_by_account(account_addr):
+  ret = req_get_json(mkurl('accounts/', account_addr, '/hotspots'))
   log.info(f"ghbo ret: {ret}")
   # cowardly refuse to return an entry if there's > 1 with the same name
   if d:= ret.get('data'):
@@ -106,17 +109,18 @@ def get_hotspot_address(hotspot_name):
   elif len(ret['data']) == 1:
     return ret['data'][0]['address']
 
-def get_hotspot_name(hotspot_address):
+def get_hotspot(hotspot_address):
   ret = req_get_json(mkurl('hotspots/', hotspot_address))
-  return ret['data']['name']
+  return ret['data']
 
-def collect_hotspots():
+def collect_hotspots_and_accounts():
   '''Using our environment config, collect all the hotspots we should
      monitor. Some of these are simply a hotspot, some are the hotspot
-     owner. In the latter case, we should rescan it occasionally (say,
+     account. In the latter case, we should rescan it occasionally (say,
      hourly).
   '''
 
+  collectables = {'hotspots': {}, 'accounts': []}
   hotspot_addresses = []
   if hnames := os.environ.get('HOTSPOT_NAMES', ''):
     log.info(f"looking up hotspots by name(s): {hnames}")
@@ -131,28 +135,34 @@ def collect_hotspots():
       ha = ha.strip()
       log.info(f"adding explicit address {ha}")
       hotspot_addresses.append(ha)
-  if oas := os.environ.get('OWNER_ADDRESSES', ''):
+  if oas := os.environ.get('ACCOUNT_ADDRESSES', ''):
     for oa in oas.split(','):
-      log.info(f"looking up hotspots by owner address {oa}")
+      log.info(f"looking up hotspots by account address {oa}")
       oa = oa.strip()
-      if haddrs := get_hotspots_by_owner(oa):
-        log.info(f"got addresses for owner address {oa}: hotspots: {haddrs}")
+      if haddrs := get_hotspots_by_account(oa):
+        log.info(f"got addresses for account address {oa}: hotspots: {haddrs}")
         hotspot_addresses.extend(haddrs)
 
   # now that we have a big list o' addresses, get their names. We might have
   # been passed a name, but we may as well standardize this.
   hotspots = {}
+  accounts = {}
   for ha in hotspot_addresses:
-    hn = get_hotspot_name(ha)
+    h = get_hotspot(ha)
+    hn = h['name']
+    ho = h['owner']
+    accounts[ho] = accounts.get(ho,0) + 1
     if not hn:
       log.error(f"could not find hotspot name for address {hn}. we'll exclude it from stats.")
       continue
     hotspots[ha] = hn
-  return hotspots
+  collectables['hotspots'] = hotspots
+  collectables['accounts'] = accounts
+  return collectables
 
 def stats_for_hotspot(addr, hname):
-  ret = req_get_json(mkurl('hotspots/', addr))
-  d = ret['data']
+  # do main stats
+  d = get_hotspot(addr)
 
   # this hotspot exists.
   HOTSPOT_UP.labels(addr,hname).set(1)
@@ -179,20 +189,63 @@ def stats_for_hotspot(addr, hname):
     haz_addr = 1
   HOTSPOT_YES_LISTEN_ADDRS.labels(addr,hname).set(haz_addr)
 
-  # slow_whatever
+  # other stats
+  hotspot_activity_counts(addr,hname)
+  # todo: hotspots/addr/witnesses
+
+  # slow stats
   slow_stats_for_hotspot(addr, hname, d)
 
-HOTSPOTS = []
+def account_activity_counts(addr):
+  cret = req_get_json(mkurl('accounts/', addr, '/activity/count'))
+  # only send some of these; not sure if they are all in use
+  for k,v, in cret['data'].items():
+    if k.startswith( ('rewards_', 'payment_', 'assert_', 'add_gateway') ):
+      log.info(f"- {k} = {v}")
+      ACCOUNT_ACTIVITY_COUNT.labels(addr,k).set(v)
+
+def hotspot_activity_counts(addr,hname):
+  cret = req_get_json(mkurl('hotspots/', addr, '/activity/count'))
+  # only send some of these; not sure if they are all in use
+  for k,v, in cret['data'].items():
+    if k.startswith( ('state_channel_', 'rewards_', 'poc_', 'consensus_', 'assert_') ):
+      HOTSPOT_ACTIVITY_COUNT.labels(addr,hname,k).set(v)
+
+def account_stats(addr):
+  aret = req_get_json(mkurl('accounts/', addr))
+  hnt_bal = 0
+  dc_bal = 0
+  if bal := aret['data']['balance']:
+    # the balance is an integer. Move the decimal. (yes, we risk losing precision)
+    hnt_bal = bal/10**8
+  if bal := aret['data']['dc_balance']:
+    # is DC too big? mine is zero
+    dc_bal = bal
+  ACCOUNT_BALANCE.labels(addr,'DC').set(dc_bal)
+  ACCOUNT_BALANCE.labels(addr,'HNT').set(hnt_bal)
+  ACCOUNT_BLOCK.labels(addr).set(aret['data']['block'])
+
+def stats_for_account(addr):
+  account_stats(addr)
+  account_activity_counts(addr)
+
+
+
+COLLECT = []
 @REQUEST_TIME.time()
 def stats():
-  global HOTSPOTS
-  if not HOTSPOTS:
+  global COLLECT
+  if not COLLECT:
     log.info("collecting hotspots.")
-    HOTSPOTS = collect_hotspots()
-    log.debug(f"created hotspot dict: {HOTSPOTS}")
+    COLLECT = collect_hotspots_and_accounts()
+    log.debug(f"created hotspot dict: {COLLECT['hotspots']}")
+    log.debug(f"created account dict: {COLLECT['accounts']}")
 
-  for addr,hname in HOTSPOTS.items():
+  for addr,hname in COLLECT['hotspots'].items():
     stats_for_hotspot(addr, hname)
+
+  for addr,_ in COLLECT['accounts'].items():
+    stats_for_account(addr)
 
 
 if __name__ == '__main__':
@@ -202,6 +255,7 @@ if __name__ == '__main__':
     try:
       stats()
     except ValueError as ex:
+      log.exception(ex)
       log.error(f"stats loop failed, {type(ex)}: {ex}")
 
     # sleep 30 seconds
